df422a0b8cbb59de84e270fbd2ab77c82136f223
LRosas93 <luisr1993@yahoo.com>
2023-03-23 09:53:20 -0700

    background images change when clicked on

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function changeBackground() {
+    var isClicked = false;
+
+    var card = document.getElementById('a1');
+    card.style.backgroundImage = "url('/assets/deer.png')";
+    card.style.backgroundSize = 'cover';
+}

eea62deb6d6098ee2fdf964ec33a0d3e6c63a34e
LRosas93 <luisr1993@yahoo.com>
2023-05-03 09:42:29 -0700

    Logic for toggling card appearance added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const deerBackground = "url('assets/deer.png')"
 
-function changeBackground() {
-    var isClicked = false;
 
-    var card = document.getElementById('a1');
-    card.style.backgroundImage = "url('/assets/deer.png')";
+// toggle button
+let activeStatus = false;
+
+function myFunction() {
+    let card = document.getElementById('card1');
+    const cardBackground = "url('assets/cardBG.png')"
+
+    if (!activeStatus) {
+        card.style.backgroundImage = deerBackground;
+        activeStatus = true;
+    } else {
+        card.style.backgroundImage = cardBackground;
+        activeStatus = false;
+    }
     card.style.backgroundSize = 'cover';
-}
+};
+

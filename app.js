1c95d091bba6dc325c6c801900b3a49ead9327f1
LRosas93 <luisr1993@yahoo.com>
2023-06-13 08:59:47 -0700

    linked javascript to html file

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-const deerBackground = "url('assets/deer.png')"
+const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-// toggle button
-let activeStatus = false;
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-function myFunction() {
-    let card = document.getElementById('card1');
-    const cardBackground = "url('assets/cardBG.png')"
+    this.classList.add('flip');
 
-    if (!activeStatus) {
-        card.style.backgroundImage = deerBackground;
-        activeStatus = true;
-    } else {
-        card.style.backgroundImage = cardBackground;
-        activeStatus = false;
-    }
-    card.style.backgroundSize = 'cover';
-};
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    } 
 
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+
+    let isMatch = firstCard.dataset.animal === secondCard.dataset.animal;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+// Executes itself after declaration; IIFE (Immediately Invoked Function Expression)
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
